b2b05202254a587ad041bc82c4cc1e24544e9b6a
AaronIyrene <aaronpsam@gmail.com>
2020-09-07 14:42:18 +0530

    modified

--- index.php
+++ index.php
@@ -1,190 +1,190 @@
 <!doctype html>
 <html lang="en">
-<head>
+ <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Pizza Restaurant</title>
       <link href="//fonts.googleapis.com/css?family=Spartan:400,500,600,700,900&display=swap" rel="stylesheet">
      <link rel="stylesheet" href="assets/css/style-starter.css">
-</head>
+ </head>
 
-<body>
+ <body>
 
-<div class="w3l-top-header">
-  <header id="site-header" class="fixed-top">
-    <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-dark stroke">
-        <a class="navbar-brand" href="index.php">
-          <span class="fa fa-pie-chart"></span> Pizza pan
-        </a>
-       
-        <button class="navbar-toggler  collapsed bg-gradient" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
-          aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
-          <span class="navbar-toggler-icon fa icon-close fa-times"></span>
-          </span>
-        </button>
+    <div class="w3l-top-header">
+    <header id="site-header" class="fixed-top">
+        <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark stroke">
+            <a class="navbar-brand" href="index.php">
+            <span class="fa fa-pie-chart"></span> Pizza pan
+            </a>
+        
+            <button class="navbar-toggler  collapsed bg-gradient" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+            <span class="navbar-toggler-icon fa icon-close fa-times"></span>
+            </span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul class="navbar-nav ml-auto">
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav ml-auto">
+                
+                <li class="nav-item @@services-active-active">
+                <a class="nav-link effect-3" href="services.php">Menu<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="px-4 fill-none">
+                <a href="cart.php" class="btn btn-style btn-secondary">Cart <span class="fa fa-shopping-cart mr-2"></span></a>
+                </li>
+            </ul>
             
-            <li class="nav-item @@services-active-active">
-              <a class="nav-link effect-3" href="services.php">Menu<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="px-4 fill-none">
-              <a href="cart.php" class="btn btn-style btn-secondary">Cart <span class="fa fa-shopping-cart mr-2"></span></a>
-              </li>
-          </ul>
-          
+            </div>
+        </nav>
         </div>
-      </nav>
+    </header>
     </div>
-  </header>
-</div>
 
-<section class="w3l-banner" id="banner">
-    <div class="new-block py-5">
-        <div class="container">
-            <div class="row middle-section">
-                <div class="col-lg-7 section-width align-self">
-                    <h5>Eat pizza everyday</h5>
-                    <h2>Share your love for pizza</h2>
-                    <a href="orderform.php" class="btn btn-secondary btn-outline-style mt-md-5 mt-4"> 
-                        <span class="fa fa-shopping-cart mr-2"></span> Add to Cart</a>
+    <section class="w3l-banner" id="banner">
+        <div class="new-block py-5">
+            <div class="container">
+                <div class="row middle-section">
+                    <div class="col-lg-7 section-width align-self">
+                        <h5>Eat pizza everyday</h5>
+                        <h2>Share your love for pizza</h2>
+                        <a href="orderform.php" class="btn btn-secondary btn-outline-style mt-md-5 mt-4"> 
+                            <span class="fa fa-shopping-cart mr-2"></span> Add to Cart</a>
+                    </div>
+                    <div class="col-lg-5 history-info mt-lg-0 mt-5 pt-lg-0 pt-md-4">
+                        <img src="assets/images/pizza-full.png" class="img-fluid" alt="image">
+                        <div class="cost">
+                            <h5>Only</h5>
+                            <h3>$25</h3>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-lg-5 history-info mt-lg-0 mt-5 pt-lg-0 pt-md-4">
-                    <img src="assets/images/pizza-full.png" class="img-fluid" alt="image">
-                    <div class="cost">
-                        <h5>Only</h5>
-                        <h3>$25</h3>
+            </div>
+    </section>
+
+    <section class="w3l-about py-5">
+        <div class="container py-lg-5 py-md-4">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h3>We make the Best Pizza in your town</h3>
+                    <h5 class="mt-lg-3 mt-2">Fusce fringilla tincidunt laoreet volutpat cras varius sit.
+                        metus vitae diam mauris.
+                    </h5>
+                    <p class="mt-sm-4 mt-3 mb-sm-0 mb-2"> Sed in metus libero. Sed volutpat eget dui ut tempus. Fusce fringilla tincidunt laoreet
+                        Morbi ac metus vitae diam scelerisque malesuada eget eu mauris. Cras varius lorem ac velit pharetra,
+                        non scelerisque mi vulputate. Phasellus bibendum.</p>
+                
+                </div>
+                <div class="col-lg-6 mt-lg-0 mt-5">
+                    <div class="row">
+                        <div class="col-6 features-with-17-left1">
+                            <a href="services.html" class="icon"><img src="assets/images/pizza.png" alt="" /></a>
+                            <h4><a href="#url">Right food baked with natural ingredient</a></h4>
+                        </div>
+                        <div class="col-6 features-with-17-left1">
+                            <a href="services.html" class="icon"><img src="assets/images/burger.png" alt="" /></a>
+                            <h4><a href="#url">The use of natural best quality products</a></h4>
+                        </div>
+                        <div class="col-6 features-with-17-left1 mt-5 pt-lg-3">
+                            <a href="services.html" class="icon"><img src="assets/images/drink.png" alt="" /></a>
+                            <h4><a href="#url"> World’s best Chef and Nutritionist!</a></h4>
+                        </div>
+                        <div class="col-6 features-with-17-left1 mt-5 pt-lg-3">
+                            <a href="services.html" class="icon"><img src="assets/images/fries.png" alt="" /></a>
+                            <h4><a href="#url">Fastest delivery on your door step</a></h4>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-</section>
+    </section>
 
-<section class="w3l-about py-5">
-    <div class="container py-lg-5 py-md-4">
-        <div class="row">
-            <div class="col-lg-6">
-                <h3>We make the Best Pizza in your town</h3>
-                <h5 class="mt-lg-3 mt-2">Fusce fringilla tincidunt laoreet volutpat cras varius sit.
-                    metus vitae diam mauris.
-                </h5>
-                <p class="mt-sm-4 mt-3 mb-sm-0 mb-2"> Sed in metus libero. Sed volutpat eget dui ut tempus. Fusce fringilla tincidunt laoreet
-                    Morbi ac metus vitae diam scelerisque malesuada eget eu mauris. Cras varius lorem ac velit pharetra,
-                    non scelerisque mi vulputate. Phasellus bibendum.</p>
-               
-            </div>
-            <div class="col-lg-6 mt-lg-0 mt-5">
-                <div class="row">
-                    <div class="col-6 features-with-17-left1">
-                        <a href="services.html" class="icon"><img src="assets/images/pizza.png" alt="" /></a>
-                        <h4><a href="#url">Right food baked with natural ingredient</a></h4>
+    <section class="w3l-portfolio-8 py-5">
+        <div class="portfolio-main py-lg-5 py-md-4">
+            <div class="container">
+                <div class="title-content text-center">
+                    <h6 class="sub-title">Gallery</h6>
+                    <h3 class="title-big">Pizza Gallery</h3>
+                </div>
+                <div class="row galler-top mt-lg-5 mt-4">
+                    <div class="col-md-3 col-6 protfolio-item hover14 align-self">
+                        <a href="assets/images/g6.jpg" data-lightbox="example-set"
+                        data-title="lorem ipsum dolor sit amet">
+                                <figure>
+                                    <img src="assets/images/g6.jpg" alt="product" class="img-fluid radius-image">
+                                </figure>
+                        </a>
                     </div>
-                    <div class="col-6 features-with-17-left1">
-                        <a href="services.html" class="icon"><img src="assets/images/burger.png" alt="" /></a>
-                        <h4><a href="#url">The use of natural best quality products</a></h4>
+                    <div class="col-md-3 col-6 protfolio-item hover14">
+                        <a href="assets/images/g1.jpg" data-lightbox="example-set" class="mb-4"
+                        data-title="lorem ipsum dolor sit amet">
+                                <figure>
+                                    <img src="assets/images/g1.jpg" alt="product" class="img-fluid radius-image">
+                                </figure>
+                        </a>
+                        <a href="assets/images/g2.jpg" data-lightbox="example-set"
+                        data-title="lorem ipsum dolor sit amet">
+                                <figure>
+                                    <img src="assets/images/g2.jpg" alt="product" class="img-fluid radius-image">
+                                </figure>
+                        </a>
                     </div>
-                    <div class="col-6 features-with-17-left1 mt-5 pt-lg-3">
-                        <a href="services.html" class="icon"><img src="assets/images/drink.png" alt="" /></a>
-                        <h4><a href="#url"> World’s best Chef and Nutritionist!</a></h4>
+                    <div class="col-md-3 col-6 protfolio-item hover14">
+                        <a href="assets/images/g3.jpg" data-lightbox="example-set" class="mb-4"
+                        data-title="lorem ipsum dolor sit amet">
+                                <figure>
+                                    <img src="assets/images/g3.jpg" alt="product" class="img-fluid radius-image">
+                                </figure>
+                        </a>
+                        <a href="assets/images/g6.jpg" data-lightbox="example-set"
+                        data-title="lorem ipsum dolor sit amet">
+                                <figure>
+                                    <img src="assets/images/g6.jpg" alt="product" class="img-fluid radius-image">
+                                </figure>
+                        </a>
                     </div>
-                    <div class="col-6 features-with-17-left1 mt-5 pt-lg-3">
-                        <a href="services.html" class="icon"><img src="assets/images/fries.png" alt="" /></a>
-                        <h4><a href="#url">Fastest delivery on your door step</a></h4>
+                    <div class="col-md-3 col-6 protfolio-item hover14 align-self">
+                        <a href="assets/images/g1.jpg" data-lightbox="example-set"
+                        data-title="lorem ipsum dolor sit amet">
+                                <figure>
+                                    <img src="assets/images/g1.jpg" alt="product" class="img-fluid radius-image">
+                                </figure>
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<section class="w3l-portfolio-8 py-5">
-    <div class="portfolio-main py-lg-5 py-md-4">
-        <div class="container">
-            <div class="title-content text-center">
-                <h6 class="sub-title">Gallery</h6>
-                <h3 class="title-big">Pizza Gallery</h3>
-            </div>
-            <div class="row galler-top mt-lg-5 mt-4">
-                <div class="col-md-3 col-6 protfolio-item hover14 align-self">
-                    <a href="assets/images/g6.jpg" data-lightbox="example-set"
-                    data-title="lorem ipsum dolor sit amet">
-                            <figure>
-                                <img src="assets/images/g6.jpg" alt="product" class="img-fluid radius-image">
-                            </figure>
-                    </a>
-                </div>
-                <div class="col-md-3 col-6 protfolio-item hover14">
-                    <a href="assets/images/g1.jpg" data-lightbox="example-set" class="mb-4"
-                    data-title="lorem ipsum dolor sit amet">
-                            <figure>
-                                <img src="assets/images/g1.jpg" alt="product" class="img-fluid radius-image">
-                            </figure>
-                    </a>
-                    <a href="assets/images/g2.jpg" data-lightbox="example-set"
-                    data-title="lorem ipsum dolor sit amet">
-                            <figure>
-                                <img src="assets/images/g2.jpg" alt="product" class="img-fluid radius-image">
-                            </figure>
-                    </a>
-                </div>
-                <div class="col-md-3 col-6 protfolio-item hover14">
-                    <a href="assets/images/g3.jpg" data-lightbox="example-set" class="mb-4"
-                    data-title="lorem ipsum dolor sit amet">
-                            <figure>
-                                <img src="assets/images/g3.jpg" alt="product" class="img-fluid radius-image">
-                            </figure>
-                    </a>
-                    <a href="assets/images/g6.jpg" data-lightbox="example-set"
-                    data-title="lorem ipsum dolor sit amet">
-                            <figure>
-                                <img src="assets/images/g6.jpg" alt="product" class="img-fluid radius-image">
-                            </figure>
-                    </a>
-                </div>
-                <div class="col-md-3 col-6 protfolio-item hover14 align-self">
-                    <a href="assets/images/g1.jpg" data-lightbox="example-set"
-                    data-title="lorem ipsum dolor sit amet">
-                            <figure>
-                                <img src="assets/images/g1.jpg" alt="product" class="img-fluid radius-image">
-                            </figure>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="w3l-footers-20">
-    <div class="footers20">
-        <div class="container">
-            <div class="footers20-content">
-                <div class="d-grid grid-col-4 grids-content">
-                    <div class="column">
-                        <h4>Opening hours</h4>
-                        <p>Monday - Friday</p>
-                        <p class="mt-2">09:00 am to 10:00 pm</p>
+    <section class="w3l-footers-20">
+        <div class="footers20">
+            <div class="container">
+                <div class="footers20-content">
+                    <div class="d-grid grid-col-4 grids-content">
+                        <div class="column">
+                            <h4>Opening hours</h4>
+                            <p>Monday - Friday</p>
+                            <p class="mt-2">09:00 am to 10:00 pm</p>
+                        </div>
+                        
                     </div>
-                    
-                </div>
-                <div class="d-grid grid-col-3 grids-content1 bottom-border">
-                    <div class="columns copyright-grid">
-                        <p class="copy-footer-29">© 2020 Pizza Pan. All rights reserved </p>
-                    </div>
-                    <div class="columns text-right social-grid">
-                        <ul class="social">
-                            <li><a href="#url"><span class="fa fa-facebook" aria-hidden="true"></span></a></li>
-                            <li><a href="#url"><span class="fa fa-instagram" aria-hidden="true"></span></a></li>
-                            <li><a href="#url"><span class="fa fa-twitter" aria-hidden="true"></span></a></li>
-                        </ul>
+                    <div class="d-grid grid-col-3 grids-content1 bottom-border">
+                        <div class="columns copyright-grid">
+                            <p class="copy-footer-29">© 2020 Pizza Pan. All rights reserved </p>
+                        </div>
+                        <div class="columns text-right social-grid">
+                            <ul class="social">
+                                <li><a href="#url"><span class="fa fa-facebook" aria-hidden="true"></span></a></li>
+                                <li><a href="#url"><span class="fa fa-instagram" aria-hidden="true"></span></a></li>
+                                <li><a href="#url"><span class="fa fa-twitter" aria-hidden="true"></span></a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
   
   <button onclick="topFunction()" id="movetop" title="Go to top">
   	&#10548;
@@ -362,3 +362,4 @@
   </body>
 
   </html>
+ 
